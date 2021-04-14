befc34295cbb064d589c6a90cd809214e34f6d98
Mila Frerichs <mila.frerichs@gmail.com>
2021-04-14 10:32:15 +0200

    feat: make test work for triaging issues
    change the test to use graphql with nock and adjust the fixture
    add gh workflow ci action as well

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const queries = require('./queries');
 
 module.exports = (app) => {
   app.on("issues.opened", async (context) => {
-    const config = await context.config('triaging.yml', {
-      owner: context.payload.organization.login || context.payload.sender.login || '',
-      projectName: "Test",
-      column: 1
-    });
     try {
+      const config = await context.config('triaging.yml', {
+        owner: context.payload.organization.login || context.payload.sender.login || '',
+        projectName: "Test",
+        column: 1
+      });
       const data = await context.octokit.graphql(queries.findProjectColumnByNumber, {
         "owner": config.owner,
         "projectName": config.projectName,
@@ -19,7 +19,7 @@ module.exports = (app) => {
       const { node: { columns: { edges: nodes } } } = board;
       const column = nodes[0]
       const { node: { id: projectColumnId } } = column;
-      context.octokit.graphql(mutation.addProjectCard, {
+      await context.octokit.graphql(mutation.addProjectCard, {
         issue: {
           contentId: context.payload.issue.node_id,
           projectColumnId
